dcc996fedd89ad1c1fec51f0ee269d4a3cf66300
dude29 <6997743+Dude29@users.noreply.github.com>
2020-07-11 03:41:03 +0100

    Add functionality to send email using sendmail

--- checktemp.sh
+++ checktemp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 TEMP_C=$(cat /sys/class/thermal/thermal_zone0/temp | awk '{print $1 / 1000}' | sed 's/,/./');
-# TEMP_C=22.5
+# TEMP_C=99.5
 
 # Workaround to bash' lack of float comparison
 # Defer the float comparison to awk which then outputs 1 if the temp is higher than 70, or 0 otherwise
@@ -9,6 +9,7 @@ TEMP_C=$(cat /sys/class/thermal/thermal_zone0/temp | awk '{print $1 / 1000}' | s
 
 if [ $(awk 'BEGIN {print ('$TEMP_C' >= 70)}') -eq 1 ]; then
     echo "mans hot"
+	sed -e 's/--TEMP_C--/'$TEMP_C'/g' email.txt | sendmail -au $FROM -ap $PWRD $TO
 else 
     echo "mans not hot"
 fi
